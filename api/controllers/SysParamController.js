bb1c9afe7011d1c9be2362aa60d6392171769df1
username <username>
2016-01-12 15:46:08 +0000

    系统参数表

---
+++ api/controllers/SysParamController.js
@@ -0,0 +1,11 @@
+/**
+ * SysParamController
+ *
+ * @description :: Server-side logic for managing Sysparams
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
