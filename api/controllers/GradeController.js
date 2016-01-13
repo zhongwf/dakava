df8abbf867d69383cb16b72c41516cd741a6bac7
username <username>
2016-01-13 01:03:29 +0000

    更新

---
+++ api/controllers/GradeController.js
@@ -0,0 +1,11 @@
+/**
+ * GradeController
+ *
+ * @description :: Server-side logic for managing Grades
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
