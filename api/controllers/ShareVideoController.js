edcb352fccfd67e7ecf13093651fb6478174dcfd
username <username>
2016-01-13 15:33:01 +0000

    update

---
+++ api/controllers/ShareVideoController.js
@@ -0,0 +1,11 @@
+/**
+ * ShareVideoController
+ *
+ * @description :: Server-side logic for managing Sharevideos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
